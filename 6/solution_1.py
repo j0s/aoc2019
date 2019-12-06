146d80c6a8f6f1a55a2502c1d5d589598a1ce5e0
Johan Sandelin <johan.sandelin@skf.com>
2019-12-06 12:17:29 +0100

    Add day 6 solutions

---
+++ 6/solution_1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import sys
+import anytree
+
+nodes = {}
+
+def add_node(node_id, parent_id):
+    node = nodes.get(node_id, anytree.Node(node_id))
+    parent = nodes.get(parent_id, anytree.Node(parent_id))
+    node.parent = parent
+    nodes[node_id], nodes[parent_id] = node, parent
+    return node
+
+def count_orbits(tree):
+    return sum([node.depth for node in tree.descendants])
+
+if __name__ == '__main__':
+    last_node = None
+    with open('input', 'r') as input_data:
+        for line in input_data:
+            parent_id, child_id = line.strip().split(')')
+            last_node = add_node(child_id, parent_id)
+
+    print(count_orbits(last_node.root))
+

ef3f59a139b86ca5ef78d471ca16c906d44a6979
Johan Sandelin <johan@sandelin.nu>
2019-12-11 08:37:03 +0100

    Track shot asteriods, pep fixes

--- 10/solution_1.py
+++ 10/solution_1.py
@@ -9,12 +9,14 @@ def log(msg):
     if debug:
         logfile.write(msg+'\n')
 
+
 def parse_input(filename):
     output = []
     with open(filename, 'r') as input_data:
         for line in input_data:
             output.append([1 if char == '#' else 0 for char in line])
     return output
+
 
 def parse_asteroids(filename):
     output = []
@@ -24,15 +26,19 @@ def parse_asteroids(filename):
     print(output)
     return output
 
+
 def num_asteroids_visible_from(asteroids, x, y):
-    visible_asteroids = {}
+    visible_asteroids = set()
     for asteroid_x, asteroid_y in asteroids:
         if asteroid_x == x and asteroid_y == y:
             continue
+        # Asteroids blocking each other are at the same angle,
+        # so just add the angle to the set
         angle = math.atan2(asteroid_x - x, asteroid_y - y)
-        visible_asteroids[angle] = (asteroid_x, asteroid_y)
+        visible_asteroids.add(angle)
 
     return len(visible_asteroids)
+
 
 if __name__ == '__main__':
     debug = '--debug' in sys.argv
@@ -43,4 +49,5 @@ if __name__ == '__main__':
 
     asteroids = parse_asteroids('input')
 
-    print(max([num_asteroids_visible_from(asteroids, x, y) for (x, y) in asteroids]))
+    print(max([num_asteroids_visible_from(asteroids, x, y)
+               for (x, y) in asteroids]))
--- 10/solution_2.py
+++ 10/solution_2.py
@@ -9,6 +9,7 @@ def log(msg):
     if debug:
         logfile.write(msg+'\n')
 
+
 def parse_input(filename):
     output = []
     with open(filename, 'r') as input_data:
@@ -16,23 +17,33 @@ def parse_input(filename):
             output.append([1 if char == '#' else 0 for char in line])
     return output
 
+
 def parse_asteroids(filename):
     output = []
     with open(filename, 'r') as input_data:
         for y, line in enumerate(input_data):
             output += [(x, y) for x, char in enumerate(line) if char == '#']
-    print(output)
     return output
 
-def num_asteroids_visible_from(asteroids, x, y):
+
+def asteroids_visible_from(asteroids, x, y):
     visible_asteroids = {}
     for asteroid_x, asteroid_y in asteroids:
         if asteroid_x == x and asteroid_y == y:
             continue
-        angle = math.atan2(asteroid_x - x, asteroid_y - y)
-        visible_asteroids[angle] = (asteroid_x, asteroid_y)
+        # use negative y axis as 0, so when I sort by angle,
+        # the asteroids closest to the positive y axis when
+        # rotating clockwise, come first
+        angle = -math.atan2(asteroid_x - x, y - asteroid_y)
+        #angle = math.atan2(asteroid_y - y, asteroid_x - x)
+        visible_asteroids.setdefault(
+            angle, []).append((asteroid_x, asteroid_y))
 
-    return len(visible_asteroids)
+    for angle in visible_asteroids.keys():
+        visible_asteroids[angle].sort(key=lambda asteroid: math.sqrt(
+            math.pow(asteroid[0] - x, 2) + math.pow(asteroid[1] - y, 2)))
+    return visible_asteroids
+
 
 if __name__ == '__main__':
     debug = '--debug' in sys.argv
@@ -45,8 +56,19 @@ if __name__ == '__main__':
 
     best_spot = (0, 0, 0)
     for x, y in asteroids:
-        score = num_asteroids_visible_from(asteroids, x, y)
+        score = len(asteroids_visible_from(asteroids, x, y))
         if score > best_spot[0]:
             best_spot = (score, x, y)
-            
-    print(best_spot)
+
+    print(f'best spot: {best_spot}')
+    asteroids_to_shoot = asteroids_visible_from(
+        asteroids, best_spot[1], best_spot[2])
+    print(asteroids_to_shoot)
+    asteroids_shot = []
+    while len(asteroids_shot) < 200:
+        for angle in sorted(asteroids_to_shoot):
+            if len(asteroids_to_shoot[angle]) == 1:
+                asteroids_shot.append(asteroids_to_shoot.pop(angle)[0])
+            else:
+                asteroids_shot.append(asteroids_to_shoot[angle].pop(0))
+    print(asteroids_shot)
